053fefc72ef3f73fa9bf658a379a2153490becd1
breman <to.breman@gmail.com>
2016-10-19 15:42:54 +0300

    clean-up frontend

--- apps/frontend/controllers/IndexController.php
+++ apps/frontend/controllers/IndexController.php
@@ -3,28 +3,19 @@
 namespace Kladr\Frontend\Controllers {
 
     use \Phalcon\Tag,
-        \Phalcon\Mvc\Controller,
-        \Kladr\Frontend\Models\Users;
+        \Phalcon\Mvc\Controller;
 
     class IndexController extends Controller
     {
 
         public function initialize()
         {
-            Tag::setTitle(' Кладр, ФИАС в облаке.');
-            $this->view->setVar('description', 'Обработанные базы адресов «КЛАДР» и «ФИАС» с удобным и простым доступом. Возможность скачать базы КЛАДР и ФИАС');
-            $this->view->setVar('keywords', 'КЛАДР 2014, КЛАДР, ФИАС, скачать КЛАДР, скачать ФИАС, скачать базу КЛАДР, скачать базу ФИАС, доступ к базе КЛАДР, доступ к базе ФИАС, КЛАДР онлайн, ФИАС онлайн, структура базы КЛАДР, структура базы ФИАС, описание базы КЛАДР, описание базы ФИАС');
-	    
-	    $this->view->setVar('page', 'index');
-	    $this->view->setVar('hideSmallHeader', true);
-
+            Tag::setTitle('Wha?');
         }
 
         public function indexAction()
         {
-            $id = $this->session->get('user');
-            $user = $id ? Users::findById($id) : null;
-            $this->view->setVar("authorized", $user ? true : false);
+            //
         }
 
         public function show404Action()
