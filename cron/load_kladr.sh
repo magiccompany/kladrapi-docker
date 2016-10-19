608edb08f931bed809ac86377f2b6c57e02d7f33
breman <to.breman@gmail.com>
2016-10-19 03:58:21 +0300

    loader bash script for cron

---
+++ cron/load_kladr.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if_error_echo () {
+    if [ $? -gt 0 ];then
+		echo $1
+		exit 1
+	fi
+}
+
+cd ..; mkdir tmp; cd tmp
+curl -s "http://www.gnivc.ru/html/gnivcsoft/KLADR/Base.7z" > kladr.7z
+if_error_echo "load archive is failed"
+
+7z e -y kladr.7z && rm kladr.7z
+if_error_echo "extracting failed"
+
+dbf2csv -o ../files && cd .. && rm -rf tmp
+
+php -f loader/index.php
+if_error_echo "executing php script failed"
